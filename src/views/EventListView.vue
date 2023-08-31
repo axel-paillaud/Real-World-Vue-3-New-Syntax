8563807506449a65e865e9e10e0a98b1f1f7f0b0
axel-paillaud <contact@axelweb.fr>
2023-08-31 18:55:35 +0200

    put axios inside service folder, make it reusable

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,13 +1,12 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
 import { ref, onMounted } from 'vue'
-import axios from 'axios';
+import EventService from '@/services/EventService.js'
 
 const events = ref(null);
 
 onMounted(() => {
-  axios
-    .get('http://my-json-server.typicode.com/axel-paillaud/Real-World-Vue-3-New-Syntax/events')
+    EventService.getEvents()
     .then(response => {
       events.value = response.data;
     })
